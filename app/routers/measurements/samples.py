573a067a4b5b3a053a33dde04d663647035c728b
Matt Broadbent <matt.broadbent@sruc.ac.uk>
2025-05-02 16:32:19 +0100

    refactor: moving samples.py to common functions

--- app/routers/measurements/samples.py
+++ app/routers/measurements/samples.py
@@ -14,6 +14,7 @@ An on-farm example is an outdoor temperature sample.
 This collection of endpoints allows for the addition, deletion
 and finding of those samples.
 """
+
 import pymongo
 
 from fastapi import status, HTTPException, Response, APIRouter, Request
@@ -23,6 +24,7 @@ from typing import Optional, List
 from typing_extensions import Annotated
 from datetime import datetime
 from bson.objectid import ObjectId
+from ..ftCommon import FTModel, modifiedFilter
 
 router = APIRouter(
     prefix="/samples",
@@ -34,27 +36,34 @@ router = APIRouter(
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
-class Sample(BaseModel):
-    id: Optional[PyObjectId] = Field(
-        alias="_id",
+class Sample(FTModel):
+    sensor: PyObjectId = Field(
+        json_schema_extra={
+            "description": "ObjectID of sensor",
+            "example": str(ObjectId()),
+        }
+    )
+    timestamp: Optional[datetime] = Field(
         default=None,
         json_schema_extra={
-            'description': 'ObjectID of sensor',
-            'example': str(ObjectId())
-        })
-    sensor: PyObjectId = Field(json_schema_extra={
-        'description': 'ObjectID of sensor',
-        'example': str(ObjectId())})
-    timestamp:  Optional[datetime] = Field(default=None, json_schema_extra={
-        'description': 'Time when sample recorded. Current time inserted'
-                       + ' if empty',
-        'example': str(datetime.now())})
-    value: float = Field(json_schema_extra={
-        'description': 'Value recorded by sensor',
-        'example': 4.3})
-    predicted: Optional[bool] = Field(default=False, json_schema_extra={
-        'description': 'Flag if the value is a predicted value or not',
-        'example': True})
+            "description": "Time when sample recorded. Current time inserted"
+            + " if empty",
+            "example": str(datetime.now()),
+        },
+    )
+    value: float = Field(
+        json_schema_extra={
+            "description": "Value recorded by sensor",
+            "example": 4.3,
+        }
+    )
+    predicted: Optional[bool] = Field(
+        default=False,
+        json_schema_extra={
+            "description": "Flag if the value is a predicted value or not",
+            "example": True,
+        },
+    )
 
 
 class SampleCollection(BaseModel):
@@ -68,7 +77,7 @@ class SampleCollection(BaseModel):
     status_code=status.HTTP_201_CREATED,
     response_model_by_alias=False,
 )
-async def create_samples(request: Request, samples: Sample):
+async def create_sample(request: Request, sample: Sample):
     """
     Create a new sample if it does not already exist.
 
@@ -77,37 +86,43 @@ async def create_samples(request: Request, samples: Sample):
 
     :param sample: Sample to be added
     """
-    model = samples.model_dump(by_alias=True, exclude=["id"])
+    sample.created = datetime.now()
+    model = sample.model_dump(by_alias=True, exclude=["ft", "modified"])
     if model["timestamp"] is None:
         model["timestamp"] = datetime.now()
     try:
-        new_samples = await request.app.state.samples.insert_one(model)
+        new_sample = await request.app.state.samples.insert_one(model)
     except pymongo.errors.DuplicateKeyError:
-        raise HTTPException(status_code=404,
-                            detail=f"Sample {samples} already exists")
+        raise HTTPException(
+            status_code=404, detail=f"Sample {sample} already exists"
+        )
     if (
-        created_samples := await
-        request.app.state.samples.find_one({"_id": new_samples.inserted_id})
+        created_sample := await request.app.state.samples.find_one(
+            {"_id": new_sample.inserted_id}
+        )
     ) is not None:
-        return created_samples
-    raise HTTPException(status_code=404, detail=f"Sample {samples._id}" +
-                        " not successfully added")
+        return created_sample
+    raise HTTPException(
+        status_code=404,
+        detail=f"Sample {sample._id}" + " not successfully added",
+    )
 
 
-@router.delete("/{id}", response_description="Delete a samples")
-async def remove_samples(request: Request, id: str):
+@router.delete("/{ft}", response_description="Delete a samples")
+async def remove_samples(request: Request, ft: str):
     """
     Delete a sample.
 
-    :param id: ObjectID of the sample to delete
+    :param ft: ObjectID of the sample to delete
     """
     delete_result = await request.app.state.samples.delete_one(
-        {"_id": ObjectId(id)})
+        {"_id": ObjectId(ft)}
+    )
 
     if delete_result.deleted_count == 1:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-    raise HTTPException(status_code=404, detail=f"Sample {id} not found")
+    raise HTTPException(status_code=404, detail=f"Sample {ft} not found")
 
 
 @router.get(
@@ -116,32 +131,32 @@ async def remove_samples(request: Request, id: str):
     response_model=SampleCollection,
     response_model_by_alias=False,
 )
-async def sample_query(request: Request,
-                       id: str | None = None,
-                       sensor: str | None = None,
-                       start: datetime | None = datetime(1970, 1, 1, 0, 0, 0),
-                       end: datetime | None = None,
-                       predicted: bool | None = False):
-    """
-    Search for a sample given the provided criteria.
-
-    :param id: Object ID of the sample
-    :param sensor: Object ID of the sensor which generated this sample
-    :param start: Timestamp from which to start the search
-    :param end: Timestamp from which to end the search
-    :param predicted: Whether or not the value is real or predicted
-    """
-    if id:
-        id = ObjectId(id)
+async def sample_query(
+    request: Request,
+    ft: str | None = None,
+    sensor: str | None = None,
+    start: datetime | None = datetime(1970, 1, 1, 0, 0, 0),
+    end: datetime | None = None,
+    predicted: bool | None = False,
+    createdStart: datetime | None = datetime(1970, 1, 1, 0, 0, 0),
+    createdEnd: datetime | None = None,
+    modifiedStart: datetime | None = None,
+    modifiedEnd: datetime | None = None,
+):
+    """Search for a sample given the provided criteria."""
+    if ft:
+        ft = ObjectId(ft)
+    if not createdEnd:
+        createdEnd = datetime.now()
+    mod = modifiedFilter(modifiedStart, modifiedEnd)
     if not end:
         end = datetime.now()
-    query = {
-        "_id": id,
-        "sensor": sensor,
-        "predicted": predicted
-        }
+    query = {"_id": ft, "sensor": sensor, "predicted": predicted}
     filtered_query = {k: v for k, v in query.items() if v is not None}
     filtered_query["timestamp"] = {"$gte": start, "$lte": end}
+    filtered_query["created"] = {"$gte": createdStart, "$lte": createdEnd}
+    if mod.search:
+        filtered_query["modified"] = {"$gte": mod.start, "$lte": mod.end}
     result = await request.app.state.samples.find(filtered_query).to_list(1000)
     if len(result) > 0:
         return SampleCollection(samples=result)
--- tests/test_samples.py
+++ tests/test_samples.py
@@ -5,10 +5,10 @@ class TestSamples:
         response_json = response.json()
         assert response.status_code == 201
 
-        sample_id = response_json['id']
+        sample_id = response_json['ft']
 
         response = test_client.get(
-            f"/measurements/samples/?id={sample_id}")
+            f"/measurements/samples/?ft={sample_id}")
         assert response.status_code == 200
         assert len(response.json()["samples"]) == 1
         response_json = response.json()["samples"][0]
@@ -27,19 +27,19 @@ class TestSamples:
         response_json = response.json()
         assert response.status_code == 201
 
-        sample_id = response_json['id']
+        sample_id = response_json['ft']
 
         response = test_client.delete(
             f"/measurements/samples/{sample_id}")
         assert response.status_code == 204
 
         response = test_client.get(
-            f"/measurements/samples/?id={sample_id}")
+            f"/measurements/samples/?ft={sample_id}")
         assert response.status_code == 404
 
     def test_get_sample_not_found(self, test_client, object_id):
         response = test_client.get(
-            f"/measurements/samples/?id={object_id}")
+            f"/measurements/samples/?ft={object_id}")
         assert response.status_code == 404
 
     def test_create_sample_wrong_payload(self, test_client):
